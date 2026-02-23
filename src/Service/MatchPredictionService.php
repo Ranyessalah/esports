58419f03a976a858072db9018496867f8bfcfd9c
Ranyessalah <164952114+Ranyessalah@users.noreply.github.com>
2026-02-23 03:16:24 +0100

    fixed prediction model

--- src/Service/MatchPredictionService.php
+++ src/Service/MatchPredictionService.php
@@ -6,15 +6,16 @@ class MatchPredictionService
 {
     public function predict(int $team1, int $team2): ?string
     {
-       $python = "C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python312\\python.exe";
-$script = "C:\\Users\\User\\OneDrive\\Bureau\\esports_nermine\\esports_nermine\\predict.py";
+    //    $python = "C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python312\\python.exe";
+        $python = "C:\\Users\\ranes\\AppData\\Local\\Programs\\Python\\Python312\\python.exe";
+        $script = "C:\\Users\\ranes\\OneDrive\\Desktop\\projet_3\\esports\\esports\\esports\\predict.py";
 
         $command = "\"$python\" \"$script\" $team1 $team2";
 
         $output = shell_exec($command . " 2>&1");
 
         if (!$output) {
-            return null;
+            return null;    
         }
 
         return trim($output);
